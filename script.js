5052a5918cf4604a44c3fb1d3ba9502c94b52674
unknown <rory.saxby@gmail.com>
2020-12-04 22:13:43 +0000

    initial setup

---
+++ script.js
@@ -0,0 +1,27 @@
+(() =>{
+
+    const socket = io();
+    const chatForm = {};
+
+
+    const emitMessage = (message) =>{
+        socket.emit('chat message', message);
+    }
+    
+
+    const submitChatMessage = (e) =>{
+        console.log(e);
+        console.log(chatForm.messageInput);
+    }
+
+    const setupChatForm = () =>{
+        chatForm.form = document.getElementById(`ChatForm`);
+        chatForm.messageInput = document.getElementById(`MessageInput`);
+        chatForm.form.addEventListener("submit", submitChatMessage);
+    }
+
+    const init = () =>{
+        setupChatForm();
+    };
+    init();
+})();
